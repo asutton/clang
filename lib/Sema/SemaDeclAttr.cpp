lock3/meta

Initial lifetime checker in clang

lib/Sema/SemaDeclAttr.cpp
@@ -6250,6 +6250,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case ParsedAttr::AT_Suppress:
     handleSuppressAttr(S, D, AL);
     break;
+  case ParsedAttr::AT_Owner:
+    handleSimpleAttribute<OwnerAttr>(S, D, AL);
+    break;
+  case ParsedAttr::AT_Pointer:
+    handleSimpleAttribute<PointerAttr>(S, D, AL);
+    break;
   case ParsedAttr::AT_OpenCLKernel:
     handleSimpleAttribute<OpenCLKernelAttr>(S, D, AL);
     break;

asutton/clang

Conflicts:
	lib/Sema/SemaDeclAttr.cpp

lib/Sema/SemaDeclAttr.cpp
@@ -6333,6 +6333,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case AttributeList::AT_Suppress:
     handleSuppressAttr(S, D, Attr);
     break;
+  case AttributeList::AT_Owner:
+    handleSimpleAttribute<OwnerAttr>(S, D, Attr);
+    break;
+  case AttributeList::AT_Pointer:
+    handleSimpleAttribute<PointerAttr>(S, D, Attr);
+    break;
   case AttributeList::AT_OpenCLKernel:
     handleSimpleAttribute<OpenCLKernelAttr>(S, D, Attr);
     break;