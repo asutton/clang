lock3/meta

Special case pointer assignment, unify this/args handling, support [[gsl::lifetime_const]]

lib/Sema/SemaDeclAttr.cpp
@@ -6256,6 +6256,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case ParsedAttr::AT_Pointer:
     handleSimpleAttribute<PointerAttr>(S, D, AL);
     break;
+  case ParsedAttr::AT_Lifetimeconst:
+    handleSimpleAttribute<LifetimeconstAttr>(S, D, AL);
+    break;
+  case ParsedAttr::AT_Lifetime:
+    handleSimpleAttribute<LifetimeAttr>(S, D, AL);
+    break;
   case ParsedAttr::AT_OpenCLKernel:
     handleSimpleAttribute<OpenCLKernelAttr>(S, D, AL);
     break;

asutton/clang

(cherry picked from commit 0b638c360214aaff4c6d8ad7fd22429aae12a8ff)

lib/Sema/SemaDeclAttr.cpp
@@ -6339,6 +6339,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case AttributeList::AT_Pointer:
     handleSimpleAttribute<PointerAttr>(S, D, Attr);
     break;
+  case AttributeList::AT_Lifetimeconst:
+    handleSimpleAttribute<LifetimeconstAttr>(S, D, Attr);
+    break;
+  case AttributeList::AT_Lifetime:
+    handleSimpleAttribute<LifetimeAttr>(S, D, Attr);
+    break;
   case AttributeList::AT_OpenCLKernel:
     handleSimpleAttribute<OpenCLKernelAttr>(S, D, Attr);
     break;